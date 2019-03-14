6503a6e9503e69251d01144a764797e53dcc86d1
Jaimie <dev.jaimie@gmail.com>
2019-03-14 13:08:21 +0000

    Divide code lines to function (next week)

--- haksikE/scrapy_next_page.py
+++ haksikE/scrapy_next_page.py
@@ -1,47 +1,73 @@
-# 다음주 식단을 미리 크롤링함
+# Import
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from time import sleep
 import os
+import sys
 
-options = Options()
-options.headless = True
+# Function
+def get_options():
+    options = Options()
+    options.headless = True
+    return options
 
-driver = webdriver.Firefox(options=options)
-driver.implicitly_wait(5)
-driver.get('http://www.duksung.ac.kr/diet/schedule.do?menuId=1151')
-driver.implicitly_wait(5)
+def get_html(driver, url):
+    driver.implicitly_wait(5)
+    driver.get(url)
+    driver.implicitly_wait(5)
+    driver.execute_script('nextWeekday(1)')
+    sleep(3)
 
-driver.execute_script('nextWeekday(1)')
-sleep(3)
+    html = driver.page_source
+    return html
 
-html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
-contents_div = soup.find(id='schedule-table')
+def find_table(html):
+    soup = BeautifulSoup(html, 'lxml')
+    target_table = soup.find(id='schedule-table')
+    return target_table
 
-info_str = contents_div.find_all("th")
-temp_str = contents_div.find_all("td")
+def get_info(target_table):
+    info_str = target_table.find_all("th")
+    for i in range(len(info_str)):
+        info_str[i] = info_str[i].get_text('\n')+'\r\r'
+    return info_str
+   
+def get_meal(target_table):
+    meal_str = target_table.find_all("td")
+    for i in range(len(meal_str)):
+        meal_str[i] = meal_str[i].get_text('\n')+'\r\r'
+    return meal_str
 
-for i in range(len(temp_str)):
-    temp_str[i] = temp_str[i].get_text('\n')+'\r\r'
-    # get_text('\n')이 <br /> 코드를 '\n'으로 바꿔줌
+def go_crawl():
+    if os.path.exists('week_meal.txt'):
+        os.remove('week_meal.txt')
+    if os.path.exists('week_info.txt'):
+        os.remove('week_info.txt')
+        
+    url = 'http://www.duksung.ac.kr/diet/schedule.do?menuId=1151'
+    options = get_options()
+    driver = webdriver.Firefox(options=options)
+    
+    try:
+        html = get_html(driver, url)
+    except:
+        sys.exit()
+        
+    target_table = find_table(html)
+    info_str = get_info(target_table)
+    meal_str = get_meal(target_table)
+        
+    meal_fp = open('week_meal.txt', 'w', encoding='utf-8')
+    meal_fp.writelines(meal_str)
+    meal_fp.close()
+    info_fp = open('week_info.txt', 'w', encoding='utf-8')
+    info_fp.writelines(info_str)
+    info_fp.close()
+    
+    driver.close()
 
-for j in range(len(info_str)):
-    info_str[j] = info_str[j].get_text('\n')+'\r\r'
 
-if os.path.exists('week_meal.txt'):
-    os.remove('week_meal.txt')
-
-meal_fp = open('week_meal.txt', 'w', encoding='utf-8')
-meal_fp.writelines(temp_str)
-meal_fp.close()
-
-if os.path.exists('week_info.txt'):
-    os.remove('week_info.txt')
-
-info_fp = open('week_info.txt', 'w', encoding='utf-8')
-info_fp.writelines(info_str)
-info_fp.close()
-
-driver.close()
+# Main
+if __name__ == '__main__':
+    go_crawl()
